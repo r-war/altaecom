52093514d2e26771102f75f3f715b4a7c4d26ac5
Rivaldi Warman <rivwar25@gmail.com>
2021-10-23 07:00:58 +0700

    add config

--- config/config.go
+++ config/config.go
@@ -3,10 +3,72 @@ package config
 import (
 	"AltaEcom/modules/migration"
 	"fmt"
+	"sync"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+type AppConfig struct {
+	AppPort        int    `mapstructure:"app_port"`
+	AppEnvironment string `mapstructure:"app_environment"`
+	DbDriver       string `mapstructure:"db_driver"`
+	DbAddress      string `mapstructure:"db_address"`
+	DbPort         int    `mapstructure:"db_port"`
+	DbUsername     string `mapstructure:"db_username"`
+	DbPassword     string `mapstructure:"db_password"`
+	DbName         string `mapstructure:"db_name"`
+}
+
+var lock = &sync.Mutex{}
+var appConfig *AppConfig
+
+func GetConfig() *AppConfig {
+	if appConfig != nil {
+		return appConfig
+	}
+	lock.Lock()
+	defer lock.Unlock()
+
+	if appConfig != nil {
+		return appConfig
+	}
+	appConfig = initConfig()
+
+	return appConfig
+}
+
+func initConfig() *AppConfig {
+	var defaultConfig AppConfig
+
+	defaultConfig.AppPort = 8000
+	defaultConfig.AppEnvironment = ""
+	defaultConfig.DbDriver = "mongodb"
+	defaultConfig.DbAddress = "localhost"
+	defaultConfig.DbPort = 3306
+	defaultConfig.DbUsername = "root"
+	defaultConfig.DbPassword = ""
+	defaultConfig.DbName = "altaecom"
+
+	viper.AutomaticEnv()
+	viper.BindEnv("app_port")
+	viper.BindEnv("app_environment")
+	viper.BindEnv("db_driver")
+	viper.BindEnv("db_address")
+	viper.BindEnv("db_port")
+	viper.BindEnv("db_username")
+	viper.BindEnv("db_password")
+	viper.BindEnv("db_name")
+
+	var finalConfig AppConfig
+	err := viper.Unmarshal(&finalConfig)
+	if err != nil {
+		return &defaultConfig
+	}
+
+	return &finalConfig
+}
 
 func InitDB() *gorm.DB {
 
@@ -15,7 +77,7 @@ func InitDB() *gorm.DB {
 		"DB_Password": "",
 		"DB_Port":     "3306",
 		"DB_Host":     "127.0.0.1",
-		"DB_Name":     "Altaecom",
+		"DB_Name":     "altaecom",
 	}
 
 	connectString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&loc=local",
